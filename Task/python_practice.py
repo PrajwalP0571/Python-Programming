c6ab10696d6714b4dce8c25a8a5e3e7fc71963dc
PrajwalP0571 <prajwal.pawar0571@gmail.com>
2026-05-18 23:20:01 +0530

    find compound intrest

--- Task/python_practice.py
+++ Task/python_practice.py
@@ -38,3 +38,14 @@ p, t, r = 8, 6, 8
 
 res = fun(p, t, r)
 print(res)
+
+print("=================")
+
+P = 1200   
+R = 5.4    
+T = 2      
+
+A = P * (1 + R/100) ** T
+CI = A - P
+
+print("Compound interest:", CI)
