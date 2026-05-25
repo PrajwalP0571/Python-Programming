0842ff348b2eca2d47d98bdedaf90454027b2cf3
PrajwalP0571 <146669008+PrajwalP0571@users.noreply.github.com>
2026-05-25 22:11:42 +0530

    Remove unused interest calculation function

--- Task/python_practice.py
+++ Task/python_practice.py
@@ -31,13 +31,4 @@ print(math.factorial(n))
 
 print("==================")
 
-def fun(p, t, r):
-    return (p * t * r) / 100
-
-p, t, r = 8, 6, 8
-
-res = fun(p, t, r)
-print(res)
-
-print("=================")
 
